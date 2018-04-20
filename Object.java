d98d4ef12e4c3f6c4671636068c57bae0d3237b6
mateusz <mateusz.cybulski345@gmail.com>
2018-04-21 01:03:25 +0200

    add new function which read object from base and save data inside object

--- Object.java
+++ Object.java
@@ -1,3 +1,6 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Object {
 
@@ -9,16 +12,19 @@ public class Object {
 	private Type type;
 	private boolean ability;
 	
-
+	
+	public Object() {
+		
+	}
+	
 
 
 	public Object (String author, String title, int yearOfIssue, Type type) {
-		setAbility(true);
 		this.author = author;
 		this.title = title;
 		this.yearOfIssue = yearOfIssue;
 		this.type = type;
-		
+		setAbility(true);
 		
 	}
 	
@@ -29,17 +35,32 @@ public class Object {
 		String query = "INSERT INTO `" + nameDataTable + "` (`type`, `author`, `tytul`, `yearOfIssue`, `ability`) " + 
 											"VALUES "+ "('" + getType() + "', '" + getAuthor() + "', '" + getTitle() + "', '" + getYearOfIssue() + "', '" + getAbility() + "'); ";
 
-
 		String answer = getBase().sendUpdate(query);
-		
-		
-		
-		
 		
 		return answer;
 	}
 
+	public String readObject(int idObject) {
+		ArrayList<String> dataFromBase = null;
+		
+		String query = "SELECT type, author, tytul, yearOfIssue, ability FROM " + getNameDataTable() + " WHERE objectId = " + Integer.toString(idObject);
+		
 
+		dataFromBase = getBase().getRsQuery(query, 5);
+		
+
+		if(dataFromBase.size() > 0) {
+		setType(dataFromBase.get(0));
+		setAuthor(dataFromBase.get(1));
+		setTitle(dataFromBase.get(2));
+		setYearOfIssue(Integer.parseInt(dataFromBase.get(3)));
+		setAbility(Integer.parseInt(dataFromBase.get(4)));
+		}
+		if(dataFromBase.size() < 1) {
+			return null;
+		}
+		else return "data was readed";
+	}
 
 
 
@@ -62,19 +83,22 @@ public class Object {
 		return type;
 	}
 	
-
-
-
-
-
-
 	public void setType(Type type) {
 		this.type = type;
 	}
-
-
-
-
+	
+	public void setType(String tpe) {
+		
+		if(tpe.equals("Book")) {
+			this.type = Type.Book;
+		}
+		else if(tpe.equals("Film")) {
+			this.type = Type.Film;
+		}
+		else if(tpe.equals("Indefinite")) {
+			this.type = Type.Indefinite;
+		}
+	}
 
 
 	public String getAuthor() {
@@ -82,17 +106,9 @@ public class Object {
 	}
 
 
-
-
-
-
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
-
-
-
 
 
 	public String getTitle() {
@@ -122,6 +138,15 @@ public class Object {
 
 	public void setAbility(boolean ability) {
 		this.ability = ability;
+	}
+	public void setAbility(int abilit) {
+		if(abilit == 1) {
+			this.ability = true;
+		}
+		else if(abilit == 0) {
+			this.ability = false;
+		}
+		
 	}
 
 	public JDBC getBase() {
