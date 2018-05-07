05913b00453ec8ce7e6f46ae0bf7ffe8632ee346
mateusz <mateusz.cybulski345@gmail.com>
2018-05-07 23:59:17 +0200

    add new class and add function borrow and give back object from base

---
+++ LibraySystem.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+public class LibraySystem {
+	JDBC base = new JDBC();
+	 ArrayList<Student> students = new ArrayList<Student>();
+	 ArrayList<Object> objects = new ArrayList<Object>();
+	 
+	
+	
+	
+	
+	public LibraySystem() {
+  /*private int objectId;
+	private Type type;
+	private String author;
+	private String title;
+	private int yearOfIssue;
+	private boolean ability;*/
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param studentId
+	 * @param objectId
+	 * @return  "book is borrow" , "book is loan"
+	 */
+	public String borrowObject(int studentId, int objectId) {
+String query = "SELECT ability FROM object WHERE objectId = " + objectId + ";";
+		
+		String ability = base.sendQuery(query);
+
+		//System.out.println(query);
+		//System.out.println(ability);
+		
+		if(ability.equals("1")) {
+			String update = "UPDATE object SET ability = 0 WHERE objectId = "  + objectId + ";";
+			base.sendUpdate(update);
+			
+			return "book is borrow";
+		}
+		else
+			return  "book is loan";
+		
+	}
+	
+	/**
+	 * 
+	 * @param studentId
+	 * @param objectId
+	 * @return  book is give back , "book is inside"
+	 */
+	public String giveBackObject(int studentId, int objectId) {
+		String query = "SELECT ability FROM object WHERE objectId = " + objectId + ";";
+		
+		String ability = base.sendQuery(query);
+
+		//System.out.println(query);
+		//System.out.println(ability);
+		
+		if(ability.equals("0")) {
+			String update = "UPDATE object SET ability = 1 WHERE objectId = "  + objectId + ";";
+			base.sendUpdate(update);
+			
+			return "book is give back";
+		}
+		else
+			return  "book is inside";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
