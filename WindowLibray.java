232a7d7085b78e5d5e6fdea11e5a903f570537dd
CEBULION34 <zarazem34@gmail.com>
2018-05-13 11:15:05 +0200

    add class JFrame: WindowLibray

---
+++ WindowLibray.java
@@ -0,0 +1,97 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class WindowLibray {
+
+	private JFrame frame;
+	private LoginToLibray login = new LoginToLibray(this);
+	private Admin admin = null;
+	private Student student = null;
+	private boolean isAdmin;
+	private CheckLoanObjects checkObjects = null;
+	
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowLibray window = new WindowLibray();
+					window.frame.setVisible(false);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public WindowLibray() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnMenu = new JMenu("menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmSprawdzObiekty = new JMenuItem("Sprawdz obiekty");
+		mnMenu.add(mntmSprawdzObiekty);
+		mntmSprawdzObiekty.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(isAdmin == true) {
+					checkObjects = new CheckLoanObjects(admin, true);
+					checkObjects.frame.setVisible(true);
+					
+				}
+				else if(isAdmin == false) {
+					checkObjects = new CheckLoanObjects(student, false);
+					checkObjects.frame.setVisible(true);
+				}
+				
+			}
+		});
+		
+
+		login.frame.setVisible(true);
+		
+	}
+	
+	public void signed() {
+		login.end();
+		frame.setVisible(true);
+	}
+	
+	public void signed(Admin admin) {
+		this.admin = admin;
+		isAdmin = login.admin;
+		login.end();
+		frame.setVisible(true);
+	}
+	
+	public void signed(Student student) {
+		this.student = student;
+		isAdmin = login.admin;
+		login.end();
+		frame.setVisible(true);
+	}
+}
